25bba5d62434f39af7213996393c9bf15b05856d
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-06-18 07:34:45 +0800

    Update url.ts

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,3 @@
 // export const URL = 'http://localhost:3001';
-// export const URL = 'https://tofu-backend-gules.vercel.app'; // Example URL for testing
-export const URL = 'http://localhost:3001';
+export const URL = 'https://tofu-backend-gules.vercel.app'; // Example URL for testing
+// export const URL = 'http://localhost:3001';
